db7c319e9d09ea562b4a3e9c082c8a90b5d6847b
Egor Ushakov <eushakov@gmail.com>
2021-11-28 23:46:08 +0300

    Start adding Transaction context

--- src/Context/Transaction/Domain/Transaction.php
+++ src/Context/Transaction/Domain/Transaction.php
@@ -6,8 +6,38 @@ use Symfony\Component\Uid\Uuid;
 
 class Transaction
 {
-    public function __construct(private string $userId, private Money $money, private string $type)
+    private string $id;
+
+    public function __construct(private string $userId, private Money $money, private string $type, private ?\DateTimeImmutable $time = null)
     {
-        $this->id = Uuid::v4();
+        $this->id = (string)Uuid::v4();
+        if (null === $time) {
+            $this->time = new \DateTimeImmutable();
+        }
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): string
+    {
+        return $this->userId;
+    }
+
+    public function getMoney(): Money
+    {
+        return $this->money;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getTime(): ?\DateTimeImmutable
+    {
+        return $this->time;
     }
 }
