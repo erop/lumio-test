f7bf7d4458078ce2550746aaaa7511f3ce5133fa
Egor Ushakov <eushakov@gmail.com>
2021-11-29 22:54:51 +0300

    WIP

--- src/Context/Shared/Integration/Event/ThresholdCreated.php
+++ src/Context/Shared/Integration/Event/ThresholdCreated.php
@@ -3,8 +3,34 @@
 namespace App\Context\Shared\Integration\Event;
 
 use App\Context\Shared\Application\Bus\Event\IntegragionEvent;
+use App\Context\Shared\Domain\Money;
 
 class ThresholdCreated implements IntegragionEvent
 {
+    public function __construct(private string             $thresholdId,
+                                private string             $userId,
+                                private Money              $money,
+                                private \DateTimeImmutable $startingFrom)
+    {
+    }
 
+    public function getThresholdId(): string
+    {
+        return $this->thresholdId;
+    }
+
+    public function getUserId(): string
+    {
+        return $this->userId;
+    }
+
+    public function getMoney(): Money
+    {
+        return $this->money;
+    }
+
+    public function getStartingFrom(): \DateTimeImmutable
+    {
+        return $this->startingFrom;
+    }
 }
---
+++ src/Context/Shared/Integration/Event/ThresholdCreatedHandler.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Context\Shared\Integration\Event;
+
+use App\Context\Shared\Application\Bus\Event\IIntegrationEventHandler;
+
+class ThresholdCreatedHandler implements IIntegrationEventHandler
+{
+
+}
